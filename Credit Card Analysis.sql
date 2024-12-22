34bde4238d7a0326ab7cc804b5d3e6e0c7aff9cc
avishkar2002 <avishkarpatil.scoe.mech@gmail.com>
2024-12-23 01:56:59 +0530

    Add files via upload

---
+++ Credit Card Analysis.sql
@@ -0,0 +1,6 @@
+create database Credit_card;
+use Credit_card;
+select * from credit_card;
+select count("Client_Num") from credit_card;
+select * from customer;
+select count("Client_Num") from customer;
